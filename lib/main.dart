ebd05e87dff208d350921a182dc1daa29a2c8b05
Nasibali144 <abdiyevnasibali@gmail.com>
2023-10-03 15:28:01 +0500

    Commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learn_notification/service/ln_service.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await LNService.settings();
 
   runApp(const MyApp());
